61800ac3553b015c6ee06b42a7da5cb4d2e33ae9
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-07 00:29:39 +0530

    Added LinkedList and modified .gitignore

---
+++ src/com/ds/collections/LinkedListHandsOn.java
@@ -0,0 +1,51 @@
+package com.ds.collections;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListHandsOn {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> shoppingList = new LinkedList<String>();
+		// Add items to list
+		shoppingList.add("Apple");
+		shoppingList.add("Banana");
+		shoppingList.add("Pear");
+		
+		// LinkedList keeps items in the same order
+		System.out.println(shoppingList);
+		
+		// Put the index where we want the element to go
+		shoppingList.add(1, "Mango");
+		System.out.println(shoppingList);
+		
+		/* 
+		 * --> LinkedLists are quite quick at adding elements
+		 * --> LinkedLists are quicker than ArrayList for adding and
+		 *     removing elements
+		 * --> LinkedLists are bit slower when randomly accessing elements
+		 *     it has to go through and see what one comes next until it gets
+		 *     to the one at once. So ArrayList are quicker for randomly
+		 *     accessing elements
+		 * --> But LinkedLists are quicker for adding and deleting elements       
+		 *     from list.
+		 * --> LinkedList methods: addFirst, addLast, removeFirst and removeLast		 *
+		 * 
+		 */
+		
+		shoppingList.removeFirst();
+		System.out.println(shoppingList);
+		
+		// LinkedLists aren't synchronized, which is a good thing in terms of performance and memory 
+		// and things like that. But it does mean that it's not thread safe. 
+		// So if you've got multiple threads trying to modify the list at the same time, they might
+		// overwrite each other's changes.
+		
+		List<String> synchronizedList = Collections.synchronizedList(shoppingList);
+		System.out.println(synchronizedList);
+
+	}
+
+}
