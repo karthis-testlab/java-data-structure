fa823b951508156a09e0d1ae8f282e2849fc8ed4
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-06-01 01:07:23 +0530

    01/06/2024:01:07AM

---
+++ src/com/ds/collections/LinkedHashSetHandsOn.java
@@ -0,0 +1,36 @@
+package com.ds.collections;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetHandsOn {
+
+	public static void main(String[] args) {
+		
+		Set<String> linkedSet = new LinkedHashSet<String>();
+		linkedSet.add("A");
+		linkedSet.add("B");
+		linkedSet.add("C");
+		linkedSet.add("D");
+		
+		Iterator<String> iterator = linkedSet.iterator();
+		
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		System.out.println(linkedSet.add("A"));
+		
+		Set<String> linkedSet1 = new LinkedHashSet<String>();
+		linkedSet1.add("A");
+		linkedSet1.add("B");
+		linkedSet1.add("E");
+		
+		linkedSet.addAll(linkedSet1);
+		
+		System.out.println(linkedSet);
+
+	}
+
+}
