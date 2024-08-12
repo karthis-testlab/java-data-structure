f828eaebc820a067adc96aec9694544d0c53eebe
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-13 02:13:05 +0530

    13/08/2024:02:13AM

---
+++ src/com/ds/collections/practices/ListPractice3.java
@@ -0,0 +1,16 @@
+package com.ds.collections.practices;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListPractice3 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		list.subList(1, 3).clear();
+		System.out.println(list); 
+
+	}
+
+}
