aac00a30e171fa54b96ad640e22fdfd0f4962f1a
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-16 01:41:05 +0530

    16/08/2024:01:41AM

---
+++ src/com/ds/collection/set/practices/SetPractice1.java
@@ -0,0 +1,16 @@
+package com.ds.collection.set.practices;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetPractice1 {
+	
+	public static void main(String[] args) {
+		Set<String> set = new TreeSet<>();
+		set.add("B");
+		set.add("A");
+		set.add("C");
+		System.out.println(set);
+	}
+
+}
