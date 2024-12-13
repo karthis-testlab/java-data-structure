21bdfba9096cf9107d2bf6a77b99d63cd7d839c6
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-12-13 23:28:57 +0530

    13/12/2024:11:28:00PM

--- src/com/ds/collection/map/practices/ConcurrentMapPratice.java
+++ src/com/ds/collection/map/practices/ConcurrentMapPratice.java
@@ -4,7 +4,14 @@ public class ConcurrentMapPratice {
 
 	/**
 	 * 
+	 * The `ConcurrentMap` is an interface which is part of the Java Collection
+	 * Framework and is available in the `java.until.concurrent` package.
 	 * 
+	 * It extends the `Map` interface and provides thread-safe operations during
+	 * retrieval, updation, and removal of elements.
+	 * 
+	 * The benefit of using `ConcurrentMap` is that it allows high concurrency for
+	 * retrieval and updates without affecting the performance.
 	 * 
 	 */
 	public static void main(String[] args) {
